e8b57ed0b12252f844a763f6a85520cf68b70e7d
IsakGairatov <isak3141592653@gmail.com>
2024-02-27 13:56:07 +0600

    add 4 Query set methods

--- classwork/pr_02_27/Pizzaria/Restoran/views.py
+++ classwork/pr_02_27/Pizzaria/Restoran/views.py
@@ -4,3 +4,26 @@ from .models import *
 
 All_food = Food.objects.all()
 All_cat = Category.objects.all()
+
+#Queryset methods
+
+#Return Category name and list of food in Category
+def Food_from_cat(cat_id):
+    return [Category.objects.get(pk = cat_id), Food.objects.filter(cat=cat_id)]
+
+#Return list of Purchases by Customer Id
+def Client_PurchaseList(client_id):
+    return Purchase.objects.filter(pk=client_id)
+
+#Return list of PurchasedFood by Purchase id
+def Purchased_Food(P_id):
+    return PurchasedFood.objects.filter(pk=P_id)
+
+#Return Food by Id
+def GetFood(F_id):
+    return Food.objects.get(pk=F_id)
+
+
+
+
+
