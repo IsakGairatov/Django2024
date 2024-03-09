d67d87a6b5ac496f077777e5d7dff3009ba6d055
IsakGairatov <isak3141592653@gmail.com>
2024-03-09 12:47:04 +0500

    Project made

---
+++ midterm/Shop/Store/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.conf import settings
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    desc = models.TextField(blank=True)
+    cost = models.IntegerField()
+    amount = models.IntegerField()
+    available = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    desc = models.TextField(blank=True)
+    default_address = models.OneToOneField('Adress', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Info'
+
+
+class Adress(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    house = models.CharField(max_length=255)
+    index = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.user.username} adress id:{self.id}'
+
+
+class BusketItems(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    purchased = models.BooleanField(default=False)
+    purch = models.ForeignKey('Purchase', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} item id: {self.id}'
+
+class Purchase(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    adress = models.ForeignKey('Adress', on_delete=models.CASCADE)
+    cost = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.user.username} date: {self.date}'
